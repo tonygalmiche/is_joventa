b5840090c2c5f1f2b7d3453d058bee18d1d65c48
Tony Galmiche <tony.galmiche@infosaone.com>
2019-09-05 16:22:49 +0200

    ajout création des devis

--- models/is_analyse_stock.py
+++ models/is_analyse_stock.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
+import datetime
 
 class IsAnalyseStock(models.Model):
     _name='is.analyse.stock'
@@ -7,6 +8,7 @@ class IsAnalyseStock(models.Model):
     _order='product_id'
 
     product_id   = fields.Many2one('product.product', u'Article', required=True)
+    order_id     = fields.Many2one('purchase.order' , u'Commande fournisseur')
     cde_cli_s1   = fields.Float(u'Cde Client S1'       , digits=(14,0))
     cde_cli_s2   = fields.Float(u'Cde Client S2'       , digits=(14,0))
     cde_cli_s3   = fields.Float(u'Cde Client S3'       , digits=(14,0))
@@ -51,6 +53,50 @@ class IsAnalyseStock(models.Model):
                 'context':{},
                 'type': 'ir.actions.act_window',
             }
+
+
+    @api.multi
+    def creer_devis_fournisseur_action(self):
+        #for obj in self:
+        #    print(obj)
+
+
+        user = self.env['res.users'].search([('id','=',self._uid)],limit=1)[0]
+        partner = user.partner_id
+        vals={
+            'partner_id'        : partner.id,
+            'fiscal_position_id': partner.property_account_position_id.id,
+        }
+        order=self.env['purchase.order'].create(vals)
+        if order:
+            order_line_obj = self.env['purchase.order.line']
+            for obj in self:
+                obj.order_id = order.id
+                qt = -obj.reapro_28
+                if qt<0:
+                    qt=0
+                now = datetime.date.today()
+                date_planned = now.strftime('%Y-%m-%d')
+                vals={
+                    'order_id'      : order.id,
+                    'product_id'    : obj.product_id.id,
+                    'name'          : obj.product_id.name,
+                    'product_uom'   : obj.product_id.uom_id.id ,
+                    'price_unit'    : 0,
+                    'product_qty'   : qt,
+                    'date_planned'  : date_planned,
+                }
+                line=order_line_obj.create(vals)
+
+
+
+
+
+
+
+
+
+
 
 
 
